a76cd311eb91fddc5a85b6b2c358515d097f43b3
ahenteti <ahmad.henteti@gmail.com>
2021-02-13 16:41:29 +0100

    add dummy responses using config file

---
+++ src/main/java/com/github/ahenteti/dummyserver/CustomJsonMapper.java
@@ -0,0 +1,18 @@
+package com.github.ahenteti.dummyserver;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomJsonMapper extends JsonMapper {
+
+    public CustomJsonMapper() {
+        super();
+        this.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        this.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+}
--- src/main/java/com/github/ahenteti/dummyserver/InMemoryDummyServerRequestResponseStore.java
+++ src/main/java/com/github/ahenteti/dummyserver/InMemoryDummyServerRequestResponseStore.java
@@ -1,16 +1,45 @@
 package com.github.ahenteti.dummyserver;
 
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @Service
 public class InMemoryDummyServerRequestResponseStore implements IDummyServerRequestResponseStore {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InMemoryDummyServerRequestResponseStore.class);
+
     private Set<DummyServerRequestResponsePair> requestResponseList = new HashSet<>();
+
+    @Value("${dummy.responses.file}")
+    private String dummyResponsesFile;
+
+    @Autowired
+    private JsonMapper jsonMapper;
+
+    @PostConstruct
+    public void init() {
+        try (Reader reader = Files.newBufferedReader(Paths.get(dummyResponsesFile))) {
+            DummyServerRequestResponsePair[] dummyResponses = jsonMapper.readValue(reader, DummyServerRequestResponsePair[].class);
+            Arrays.asList(dummyResponses).forEach(this::add);
+        } catch (Exception e) {
+            LOGGER.error("error while reading {}", dummyResponsesFile, e);
+        }
+    }
 
     @Override
     public void add(DummyServerRequestResponsePair requestResponse) {
