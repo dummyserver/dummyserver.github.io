e248118345b5c5a65654aa16d075f1023f6af673
ahenteti <ahmad.henteti@gmail.com>
2021-03-27 14:51:28 +0100

    implement TemplateVariableConverterFactory#create using reflexions

---
+++ src/main/java/com/github/ahenteti/dummyserver/service/impl/responseformatter/NullTemplateVariableConverter.java
@@ -0,0 +1,25 @@
+package com.github.ahenteti.dummyserver.service.impl.responseformatter;
+
+public class NullTemplateVariableConverter implements ITemplateVariableConverter {
+
+
+    @Override
+    public boolean canConvert(TemplateVariable templateVariable) {
+        return false;
+    }
+
+    @Override
+    public boolean cannotConvert(TemplateVariable templateVariable) {
+        return true;
+    }
+
+    @Override
+    public String getTemplateVariableName() {
+        return "";
+    }
+
+    @Override
+    public String convert(TemplateVariable templateVariable) {
+        return "";
+    }
+}
