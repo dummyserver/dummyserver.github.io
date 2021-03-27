d634143574954ef262c4bb720aa547f339db0069
ahenteti <ahmad.henteti@gmail.com>
2021-03-27 21:15:50 +0100

    add --xml-path option to RequestBodyTemplateVariableConverter converter

--- src/main/java/com/github/ahenteti/dummyserver/service/impl/responseformatter/RequestBodyTemplateVariableConverter.java
+++ src/main/java/com/github/ahenteti/dummyserver/service/impl/responseformatter/RequestBodyTemplateVariableConverter.java
@@ -2,10 +2,7 @@ package com.github.ahenteti.dummyserver.service.impl.responseformatter;
 
 import com.jayway.jsonpath.JsonPath;
 
-import java.util.Objects;
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class RequestBodyTemplateVariableConverter extends BaseTemplateVariableConverter {
 
@@ -19,19 +16,29 @@ public class RequestBodyTemplateVariableConverter extends BaseTemplateVariableCo
         if (cannotConvert(templateVariable)) {
             return super.convert(templateVariable);
         }
-        Optional<String> jsonPath = getJsonPath(templateVariable);
+
         Optional<String> requestBody = templateVariable.getRequestBody();
-        if (!jsonPath.isPresent() || !requestBody.isPresent()) {
+        if (!requestBody.isPresent()) {
             return templateVariable.toString();
         }
-        Object value = JsonPath.read(requestBody.get(), jsonPath.get());
-        return Objects.isNull(value) ? templateVariable.toString() : value.toString();
-    }
 
-    private Optional<String> getJsonPath(TemplateVariable templateVariable) {
-        Pattern pattern = Pattern.compile("--json-path='(.+?)'");
-        Matcher matcher = pattern.matcher(templateVariable.getOptions());
-        return matcher.find() ? Optional.of(matcher.group(1)) : Optional.empty();
+        Optional<String> jsonPath = templateVariable.getOption("--json-path");
+        if (jsonPath.isPresent()) {
+            Object value = JsonPath.read(requestBody.get(), jsonPath.get());
+            if (value != null) {
+                return value.toString();
+            }
+        }
+
+        Optional<String> xmlPath = templateVariable.getOption("--xml-path");
+        if (xmlPath.isPresent()) {
+            Optional<String> value = XmlPath.read(requestBody.get(), xmlPath.get());
+            if (value.isPresent()) {
+                return value.get();
+            }
+        }
+
+        return templateVariable.toString();
     }
 
 }
--- src/main/java/com/github/ahenteti/dummyserver/service/impl/responseformatter/TemplateVariable.java
+++ src/main/java/com/github/ahenteti/dummyserver/service/impl/responseformatter/TemplateVariable.java
@@ -6,6 +6,8 @@ import org.apache.commons.io.IOUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -25,6 +27,12 @@ public class TemplateVariable {
         return options.contains(option);
     }
 
+    public Optional<String> getOption(String option) {
+        Pattern pattern = Pattern.compile(option + "='(.+?)'");
+        Matcher matcher = pattern.matcher(options);
+        return matcher.find() ? Optional.of(matcher.group(1)) : Optional.empty();
+    }
+
     public Optional<String> getRequestBody() {
         try {
             return Optional.of(IOUtils.toString(getRequest().getInputStream(), UTF_8));
@@ -32,4 +40,5 @@ public class TemplateVariable {
             return Optional.empty();
         }
     }
+
 }
